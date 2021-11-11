420044c2401b7bb1937d36de1ca27f9e37836aa8
Daniil Murashev <daniil.murashev@gmail.com>
2021-11-11 00:52:39 +0000

    Onemorestartagain

---
+++ src/main/java/com/example/main/GetitgrownApplication.java
@@ -0,0 +1,13 @@
+package com.example.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetitgrownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetitgrownApplication.class, args);
+	}
+
+}
